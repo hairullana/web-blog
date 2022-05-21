d1d9359f5042d35b94a15cf9f32123fd6a22485f
Hairul Lana <hairullana99@gmail.com>
2022-05-21 13:05:28 +0800

    back link

--- resources/js/components/posts/Create.vue
+++ resources/js/components/posts/Create.vue
@@ -2,7 +2,7 @@
   <div>
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-      <h1 class="h2">Create New Post</h1>
+      <h3><router-link :to="{ name: 'dashboard' }" style="text-decoration: none;">Dashboard</router-link> / Create New Post</h3>
     </div>
 
     <div class="col-lg-8 mb-3">
