b8a6d3be61ac71a91b0925183e36b9d5046aa1da
Hairul Lana <hairullana99@gmail.com>
2022-05-31 20:41:03 +0800

    layout create post

--- resources/js/components/posts/Create.vue
+++ resources/js/components/posts/Create.vue
@@ -8,36 +8,47 @@
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800"><router-link class="path-link" :to="{ name: 'dashboard' }">Dashboard</router-link> / <router-link class="path-link" :to="{ name: 'posts' }">Posts</router-link> / Create Post</h1>
           </div>
-          <div class="col-lg-8 mb-3">
+          <div class="col-lg-12 mb-3">
             <form @submit.prevent="PostStore">
-              <div class="mb-3">
-                <label for="title" class="form-label">Title</label>
-                <input required type="text" class="form-control" v-model="title" id="title" @keyup="createSlug" autofocus>
-                <div class="invalid-feedback" v-if="this.errors.title">
-                  {{ this.errors.title[0] }}
+              <div class="row">
+                <div class="col-lg-5 mt-auto">
+                  <img src="http://127.0.0.1:8000/img/blog-1.jpg" class="img-fluid img-thumbnail">
+                </div>
+                <div class="col-lg-7">
+                  <div class="mb-3">
+                    <label for="title" class="form-label">Title</label>
+                    <input required type="text" class="form-control" v-model="title" id="title" @keyup="createSlug" autofocus>
+                    <div class="invalid-feedback" v-if="this.errors.title">
+                      {{ this.errors.title[0] }}
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                    <label for="slug" class="form-label">Slug</label>
+                    <input required type="text" class="form-control" v-model="slug" id="slug" disabled>
+                    <div class="invalid-feedback" v-if="this.errors.slug">
+                      {{ this.errors.slug[0] }}
+                    </div>
+                  </div>
+                  <div class="mb-3 ">
+                    <label for="image" class="form-label">Image</label>
+                    <input class="form-control" type="file" id="image">
+                  </div>
+                  <div class="mb-3">
+                    <label for="category" class="form-label">Category</label>
+                    <select v-model="category_id" id="category" class="form-select">
+                      <option 
+                        v-for="category in categories" 
+                        v-bind:value="category.id"
+                        >{{ category.name }}
+                      </option>
+                    </select>
+                    <div class="invalid-feedback" v-if="this.errors.category">
+                      {{ this.errors.category[0] }}
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div class="mb-3">
-                <label for="slug" class="form-label">Slug</label>
-                <input required type="text" class="form-control" v-model="slug" id="slug">
-                <div class="invalid-feedback" v-if="this.errors.slug">
-                  {{ this.errors.slug[0] }}
-                </div>
-              </div>
-              <div class="mb-3">
-                <label for="category" class="form-label">Category</label>
-                <select v-model="category_id" id="category" class="form-select">
-                  <option 
-                    v-for="category in categories" 
-                    v-bind:value="category.id"
-                    >{{ category.name }}
-                  </option>
-                </select>
-                <div class="invalid-feedback" v-if="this.errors.category">
-                  {{ this.errors.category[0] }}
-                </div>
-              </div>
-              <div class="mb-3">
+              <div class="mb-3 mt-3">
                 <label class="form-label">Body</label>
                 <input type="hidden" id="body" v-model="body">
                 <VueTrix inputId="body" inputName="body" v-model="body" placeholder="enter your content..."/>
